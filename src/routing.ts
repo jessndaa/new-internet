eabda95deaac09a9dead49b0d51aa0310f0a64b0
jessy ndaya <ilunga.ndaya@yahoo.fr>
2019-04-10 17:42:29 +0100

    seach transit info

--- src/routing.ts
+++ src/routing.ts
@@ -21,30 +21,53 @@ export class Simple {
     }
 
     public static async wikipediaindex(req: express.Request, res: express.Response)  {
-            let cc = req.body.result.parameters.any;
-            fetch("https://fr.wikipedia.org/w/api.php?format=json&utf8=&action=query&list=search&srsearch="+cc+"&srlimit=1")
-            .then(function name(result) {
-                return result.json();
-            })
-            .then(function name(body) {
-                let wiki_res = body.query.search[0].snippet as string;
-                wiki_res = wiki_res.replace("<span class=\"searchmatch\">","").replace("</span>","")
-                .replace("<span class=\"searchmatch\">","").replace("</span>","")
-                .replace("<span class=\"searchmatch\">","").replace("</span>","")
-                .replace("<span class=\"searchmatch\">","").replace("</span>","")
-                .replace("<span class=\"searchmatch\">","").replace("</span>","");
-                let splitter = wiki_res.split(' ');
-                if(splitter[splitter.length - 1] === "homme"){
-                    wiki_res += " politique";
-                }
-                res.json({
-                    "speech": "Sélon Wikipédia, "+wiki_res
-                });
-            })
-            .catch(function name() {
+            let parameters = req.body.result.parameters;
+            let intent = req.body.result.metadata.intentName;
+            try {
+                switch (intent) {
+                    case "wiki.about.person":
+                        Simple.fetchWikiPerson(req, res, parameters);
+                        break;
+                
+                    default:
+                        res.end({
+                            "speech": "Nous n'avons pas d'information sur votre recherche."
+                        });
+                        break;
+                }                
+            } catch (error) {
                 res.end({
                     "speech": "Nous n'avons pas d'information sur votre recherche."
-                }); 
-            })
+                });
+            }
+
+
+    }
+
+    private static fetchWikiPerson(req: express.Request, res: express.Response, parameters: any) {
+        fetch("https://fr.wikipedia.org/w/api.php?format=json&utf8=&action=query&list=search&srsearch="+parameters.any+"&srlimit=1")
+        .then(function name(result) {
+            return result.json();
+        })
+        .then(function name(body) {
+            let wiki_res = body.query.search[0].snippet as string;
+            wiki_res = wiki_res.replace("<span class=\"searchmatch\">","").replace("</span>","")
+            .replace("<span class=\"searchmatch\">","").replace("</span>","")
+            .replace("<span class=\"searchmatch\">","").replace("</span>","")
+            .replace("<span class=\"searchmatch\">","").replace("</span>","")
+            .replace("<span class=\"searchmatch\">","").replace("</span>","");
+            let splitter = wiki_res.split(' ');
+            if(splitter[splitter.length - 1] === "homme"){
+                wiki_res += " politique";
+            }
+            res.json({
+                "speech": "Sélon Wikipédia, "+wiki_res
+            });
+        })
+        .catch(function name() {
+            res.end({
+                "speech": "Nous n'avons pas d'information sur votre recherche."
+            }); 
+        })  
     }
 }
